bda5a93cf087386150849167af866048da82dcfb
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-23 14:29:44 +0800

    Add examples for history API

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.alert('this is the first window.alert' + history.length + ' value for the history.length!');
 
 alert('this is the second alert box' + history.length + ' window object is implied!');
+
+// history.replaceState('hazel is cute', null, 'replaceState.html');
+
+// console.log(history.state);
+
+history.pushState(null,null,'tiffwebpage.html');
